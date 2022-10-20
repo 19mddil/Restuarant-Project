ddc99211e8e3af1477f8a19ee23b229262a4214b
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-10-20 06:31:41 +0600

    adding nav and routing

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Menu from './Menu';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 const Body = () => {
     return (
         <div>
-            <Menu />
+            <Routes>
+                <Route path='/home' element={<Home />} />
+                <Route path='/menu' element={<Menu />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/' element={<Navigate to='/home' />} />
+            </Routes>
         </div>
     );
 }

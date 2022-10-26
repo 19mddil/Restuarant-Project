7608fb24211ee6158ef5b67bf22acdcec4c28cf0
19mddilAspire <mddilshadul98@gmail.com>
2022-10-26 08:33:22 +0600

    redux in restuarent project

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 
 class Home extends Component {
+    componentDidMount() {
+        console.log("Home State: ", this.state);
+        console.log("Home Props: ", this.props);
+    }
+
     render() {
         document.title = 'Restuarent';
         return (
